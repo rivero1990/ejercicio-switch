d8eb80a477b1496607d153869e25690ed8491c40
anibal rivero <riveroleonel@gmail.com>
2023-05-05 21:19:24 -0300

    problema color fondo de pagina

--- script.js
+++ script.js
@@ -12,13 +12,20 @@ function cambioEstetica() {
         case COLOR_1:
             cuerpoDePagina.style.color = "white";
             cuerpoDePagina.style.backgroundColor = "black";
+            selectOpcionColor.style.color = "white";
+            selectOpcionColor.style.backgroundColor = "grey";
             break;
         case COLOR_2:
             cuerpoDePagina.style.color = "violet";
             cuerpoDePagina.style.backgroundColor = "orange";
+            selectOpcionColor.style.color = "violet";
+            selectOpcionColor.style.backgroundColor = "black";
+
         case COLOR_3:
             cuerpoDePagina.style.color = "pink";
-            cuerpoDePagina.style.backgroundColor = "blue";    
+            cuerpoDePagina.style.backgroundColor = "blue";
+            selectOpcionColor.style.color = "pink";
+            selectOpcionColor.style.backgroundColor = "green";
 
         default:
             break;
